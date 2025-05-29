2b586591f0eb78f6a569dbc10b74368dda426843
Bercempapatya <bercempapatya@gmail.com>
2025-05-29 18:43:39 +0300

    Update YurtİçiKargo.cs

--- KargoTakipSistemi/YurtİçiKargo.cs
+++ KargoTakipSistemi/YurtİçiKargo.cs
@@ -8,14 +8,14 @@ namespace KargoTakipSistemi
 {
     //Bu somut sınıflara ihtiyacımız vardır çünkü gerçek kargo tipleri olmak zorundadır.
     //Ve bunlar soyut sınıftan miras alırlar çünkü ortak kullanılanları kullanmaları gerekir
-   public class YurtIçiKargo :Kargo
+   public class YurtIciKargo :Kargo
     {
         public override void GonderiBilgisiYazdir() //Override çünkü abstracttan miras aldı 
         {
             //Artık burada KargoTakipNumarası,AliciAdi,GonderiDurumu bilgilerini direkt kullanabiliyoruz çünkü miras alındı.
             //Ayrıca bu bilgiler yurtiçi içinde kullandığımızdan dolayı abstract olarak aldık .
             Console.WriteLine("Yurtiçi Kargo Bilgileri :");
-            Console.WriteLine("Yurtiçi Kargo Takip Numarası : "+KargoTakipNumarası);
+            Console.WriteLine("Yurtiçi Kargo Takip Numarası : "+KargoTakipNumarasi);
             Console.WriteLine("Yurtiçi Kargo Alıcı Adı : " +AliciAdi);
             Console.WriteLine("Yurtiçi Kargonun Son Durumu : "+GonderiDurumu);
         }
