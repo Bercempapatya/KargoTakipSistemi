e96df3ad3a3fddd8514c69d7403f54cb5e6b5bcd
Bercempapatya <bercempapatya@gmail.com>
2025-05-29 18:41:02 +0300

    Update Kargo.cs

--- KargoTakipSistemi/Kargo.cs
+++ KargoTakipSistemi/Kargo.cs
@@ -9,9 +9,11 @@ namespace KargoTakipSistemi
    public abstract class Kargo //Abstract çünkü genel kullanılacak ortak isimler burada olacak diğer sınıflar miras alacaktır
     {
         // Tüm ortak olacakları bu sınıfa yazıyoruz
-        public string KargoTakipNumarası { get; set; }
+        public string KargoTakipNumarasi { get; set; }
         public string AliciAdi { get; set; }
         public Durum GonderiDurumu { get; set; } //Tüm Kargoların durum bilgisi olur o yüzden diğer sınıflarda da kullanmalıyız
+        public abstract void teslimSuresiHesapla(); //Buradan türetilecek kargoların yurt içi-dışı olma durumuna bağlı teslim suresi hesaplanır.
+        public abstract void ucretHesapla(); //Kargonun yurt içi-dışı olmak üzere farklı ücretleri bu metotla hesaplanacaktır.
 
         //Şimdi de tüm kargolarda olması gerekir ama her kargo tiipi içinde farklıdır o yüzden sadece gövdesi oluşturuyoruz
         public abstract void GonderiBilgisiYazdir();//Yani her kargo tipinde bu metot içeriği farklı olacak 
